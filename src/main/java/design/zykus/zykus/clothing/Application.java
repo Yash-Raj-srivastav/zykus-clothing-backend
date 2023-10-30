89fc0de565d84a5fc053f0615cac27a0a2e1c02b
Yash-Raj-srivastav <yrseivastav88@gmail.com>
2023-10-30 11:58:23 +0530

    added code owners

--- src/main/java/design/zykus/zykus/clothing/Application.java
+++ src/main/java/design/zykus/zykus/clothing/Application.java
@@ -2,10 +2,8 @@ package design.zykus.zykus.clothing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("design.zykus.zykus.clothing.Entity")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
