7b140777db85b6c2627b8d358210f7a18c693216
Yash-Raj-srivastav <yrseivastav88@gmail.com>
2023-11-05 13:17:59 +0530

    created password reset entity impl class

--- src/main/java/design/zykus/zykus/clothing/Entity/Orders.java
+++ src/main/java/design/zykus/zykus/clothing/Entity/Orders.java
@@ -1,10 +1,57 @@
 package design.zykus.zykus.clothing.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "orders")
 public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int orderId;
+    private int userId;
+    private String order_status_code;
+    private LocalDate date_order_placed;
+    private String order_details;
 
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getOrder_status_code() {
+        return order_status_code;
+    }
+
+    public void setOrder_status_code(String order_status_code) {
+        this.order_status_code = order_status_code;
+    }
+
+    public LocalDate getDate_order_placed() {
+        return date_order_placed;
+    }
+
+    public void setDate_order_placed(LocalDate date_order_placed) {
+        this.date_order_placed = date_order_placed;
+    }
+
+    public String getOrder_details() {
+        return order_details;
+    }
+
+    public void setOrder_details(String order_details) {
+        this.order_details = order_details;
+    }
 }
